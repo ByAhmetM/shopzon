6127265956d58c514799cd1f832ae45c681571a2
ByAhmetM <118487365+ByAhmetM@users.noreply.github.com>
2023-06-07 18:40:12 +0300

    Slider eklendi.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import SliderComp from "./../home/SliderComp";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <SliderComp />
+    </div>
+  );
 }
 
 export default Home;
