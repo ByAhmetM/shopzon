dfb8ba2f97081490decffdd7cc44ae7e0b68dff9
ByAhmetM <118487365+ByAhmetM@users.noreply.github.com>
2023-06-07 18:36:28 +0300

    Navbar ve PageContainer componentleri eklendi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
+import PageContainer from "./containers/PageContainer";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <PageContainer>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </PageContainer>
     </>
   );
 }
